9098101d0ad8878c66aa077542aae5fccdab3c8e
David Koifman <koifmanDavid123@gmail.com>
2022-06-20 17:53:52 +0600

    Add Utility Functions Files

---
+++ EmojiArt/UtilityViews.swift
@@ -0,0 +1,45 @@
+//
+//  UtilityViews.swift
+//  EmojiArt
+//
+//  Created by Work on 20/6/22.
+//
+
+import SwiftUI
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        if uiImage != nil {
+            Image(uiImage: uiImage!)
+        }
+    }
+}
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            withAnimation {
+                action()
+            }
+        } label: {
+            if title != nil && systemImage != nil {
+                Label(title!, systemImage: systemImage!)
+            } else if title != nil {
+                Text(title!)
+            } else if systemImage != nil {
+                Image(systemName: systemImage!)
+            }
+        }
+    }
+}
+
+struct IdentifiableAlert: Identifiable {
+    var id: String
+    var alert: () -> Alert
+}
