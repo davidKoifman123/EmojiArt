2c86e59f2cc8103e4d06c01f0ad10f1cf8608815
David Koifman <koifmanDavid123@gmail.com>
2022-06-20 21:28:24 +0600

    Add Drag Function

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -12,6 +12,8 @@ class EmojiArtDocument: ObservableObject {
     
     init() {
         emojiArt = EmojiArtModel()
+        emojiArt.addEmoji("🥗", at: (-200, -100), size: 80)
+        emojiArt.addEmoji("🏉", at: (50, 100), size: 40)
     }
     
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
