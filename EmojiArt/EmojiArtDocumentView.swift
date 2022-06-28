10b2b4299d0cfb9e626ecc0126f607c1846b8416
David Koifman <koifmanDavid123@gmail.com>
2022-06-28 18:41:19 +0600

    Add Double Tap Increase Function

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -23,14 +23,17 @@ struct EmojiArtDocumentView: View {
         ZStack {
             Color.white.overlay(
                 OptionalImage(uiImage: document.backgroundImage)
+                    .scaleEffect(zoomScale)
                     .position(convertFromEmojiCoordinates((0, 0), in: geometry))
             )
+                .gesture(doubleTapToZoom(in: geometry.size))
             if document.backgroundImageFetchStatus == .fetching {
-                ProgressView()
+                ProgressView().scaleEffect(2)
             } else {
                 ForEach(document.emojis) { emoji in
                     Text(emoji.text)
                         .font(.system(size: fontSize(for: emoji)))
+                        .scaleEffect(zoomScale)
                         .position(position(for: emoji, in: geometry))
                 }
             }
@@ -58,41 +61,13 @@ struct EmojiArtDocumentView: View {
                     document.addEmoji(
                         String(emoji),
                         at: convertToEmojiCoordinates(location, in: geometry),
-                        size: defaultEmojiFontSize 
+                        size: defaultEmojiFontSize / zoomScale
                     )
                 }
             }
         }
         return found
     }
-    
-    /*
-    private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
-        var found = providers.loadObjects(ofType: URL.self) { url in
-            document.setBackground(EmojiArtModel.Background.url(url.imageURL))
-        }
-        
-        if !found {
-        found = providers.loadObjects(ofType: UIImage.self) { image in
-            if let data = image.jpegData(compressionQuality: 1.0) {
-                document.setBackground(.imageData(data))
-            }
-        }
-     }
-        if !found {
-        found = providers.loadObjects(ofType: String.self) { string in
-            if let emoji = string.first, emoji.isEmoji {
-                document.addEmoji(
-                    String(emoji),
-                    at: convertToEmojiCoordinates(location, in: geometry),
-                    size: defaultEmojiFontSize
-            )
-        }
-     }
-    }
-    return found
-}
-     */
     
     private func position(for emoji: EmojiArtModel.Emoji, in geometry: GeometryProxy) -> CGPoint {
         convertFromEmojiCoordinates((emoji.x, emoji.y), in: geometry)
@@ -101,8 +76,8 @@ struct EmojiArtDocumentView: View {
     private func convertToEmojiCoordinates(_ location: CGPoint, in geometry: GeometryProxy) -> (x: Int, y: Int) {
         let center = geometry.frame(in: .local).center
         let location = CGPoint (
-            x: location.x - center.x,
-            y: location.y - center.y
+            x: (location.x - center.x) / zoomScale,
+            y: (location.y - center.y) / zoomScale
         )
         return (Int(location.x), Int(location.y))
     }
@@ -110,13 +85,32 @@ struct EmojiArtDocumentView: View {
     private func convertFromEmojiCoordinates(_ location: (x: Int, y: Int), in geometry: GeometryProxy) -> CGPoint {
         let center = geometry.frame(in: .local).center
         return CGPoint(
-            x: center.x + CGFloat(location.x),
-            y: center.y + CGFloat(location.y)
+            x: center.x + CGFloat(location.x) * zoomScale,
+            y: center.y + CGFloat(location.y) * zoomScale
         )
     }
     
     private func fontSize(for emoji: EmojiArtModel.Emoji) -> CGFloat {
         CGFloat(emoji.size)
+    }
+    
+    @State private var zoomScale: CGFloat = 1
+    
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation {
+                zoomToFit(document.backgroundImage, in: size)
+         }
+      }
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0, size.width > 0, size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            zoomScale = min(hZoom, vZoom)
+        }
     }
     
     var palette: some View {
