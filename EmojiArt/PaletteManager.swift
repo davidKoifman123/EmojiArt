bcc067e1685ba1125ceaf8ec1f99e7485c964712
David Koifman <koifmanDavid123@gmail.com>
2022-07-03 01:30:04 +0600

    Add Editing Functionality

--- EmojiArt/PaletteManager.swift
+++ EmojiArt/PaletteManager.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -16,15 +19,40 @@ struct PaletteManager: View {
             ForEach(store.palettes) { palette in
                 NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                  VStack(alignment: .leading) {
-                    Text(palette.name)
+                    Text(palette.name).font(editMode == .active ? .largeTitle : .caption)
                     Text(palette.emojis)
                }
+                 .gesture(editMode == .active ? tap : nil)
             }
           }
+            .onDelete { indexSet in
+                store.palettes.remove(atOffsets: indexSet)
+            }
+            .onMove { index, newOffset in
+                store.palettes.move(fromOffsets: index, toOffset: newOffset)
+            }
        }
           .navigationTitle("Manage Palettes")
+          .navigationBarTitleDisplayMode(.inline)
+          .toolbar {
+              ToolbarItem { EditButton() }
+              ToolbarItem(placement: .navigationBarLeading) {
+                  if presentationMode.wrappedValue.isPresented {
+                      Button("Close") {
+                          presentationMode.wrappedValue.dismiss()
+                      }
+                  }
+              }
+          }
+          .environment(\.editMode, $editMode)
      }
    }
+    
+    var tap: some Gesture {
+        TapGesture().onEnded {
+            
+        }
+    }
 }
 
 struct PaletteManager_Previews: PreviewProvider {
@@ -32,5 +60,6 @@ struct PaletteManager_Previews: PreviewProvider {
         PaletteManager()
             .previewDevice("iPad Pro (11-inch) (3rd generation)")
             .environmentObject(PaletteStore(named: "Preview"))
+            .preferredColorScheme(.light)
     }
 }
