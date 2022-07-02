7f4a981f83eb82a02566c6c37a411bc5a3970386
David Koifman <koifmanDavid123@gmail.com>
2022-07-02 16:45:37 +0600

    Add Edit Form

---
+++ EmojiArt/PaletteEditor.swift
@@ -0,0 +1,20 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Work on 2/7/22.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteEditor()
+    }
+}
