4822b477264a94d7d4b4be5ff58f273986fbe288
David Koifman <koifmanDavid123@gmail.com>
2022-07-02 20:16:13 +0600

    Add Remove Emojis Functionality

--- EmojiArt/PaletteEditor.swift
+++ EmojiArt/PaletteEditor.swift
@@ -14,6 +14,7 @@ struct PaletteEditor: View {
         Form {
             nameSection
             addEmojisSection
+            removeEmojiSection
         }
         .frame(minWidth: 300, minHeight: 350)
     }
@@ -42,6 +43,23 @@ struct PaletteEditor: View {
             .removingDuplicateCharacters
         }
     }
+    
+    var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+             .font(.system(size: 40))
+        }
+    }
 }
 
 struct PaletteEditor_Previews: PreviewProvider {
@@ -49,5 +67,7 @@ struct PaletteEditor_Previews: PreviewProvider {
         Text("Fix Me!")
         PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
             .previewLayout(.fixed(width: 300, height: 350))
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
+            .previewLayout(.fixed(width: 300, height: 600))
     }
 }
